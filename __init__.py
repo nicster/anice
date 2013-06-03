b5fbf3ce26c6b59dfe5367ca80c19ffad3bc2a9f
Hess <hess_1234@gmx.net>
2013-06-04 00:01:55 +0200

    paths

--- __init__.py
+++ __init__.py
@@ -43,8 +43,8 @@ if not os.path.exists(os.path.join(app.instance_path, 'uploads/thumbnails')) :
 
 Base = declarative_base()
 
-"""engine = create_engine('sqlite+pysqlite:///flaskr.db', echo=True, module=sqlite)"""
-engine = create_engine('mysql+mysqldb://hessiboy:Roll1679Stuhl@localhost:3306/hessiboy_anice', pool_recycle=3600)
+engine = create_engine('sqlite+pysqlite:///flaskr.db', echo=True, module=sqlite)
+"""engine = create_engine('mysql+mysqldb://hessiboy:Roll1679Stuhl@localhost:3306/hessiboy_anice', pool_recycle=3600)"""
 
 Session = sessionmaker(bind=engine)
 
@@ -223,6 +223,12 @@ class Painting(Base):
 
     def my_thumbnail_path(self):
         return os.path.join(app.instance_path, IMAGE_FOLDER, THUMBNAIL_FOLDER, self.filename)
+
+    def my_path_filename(self):
+        return self.filename
+
+    def my_thumbnail_path_filename(self):
+        return os.path.join(THUMBNAIL_FOLDER, self.filename)
 
     def upload(self, file):
         file.save(self.my_path())
